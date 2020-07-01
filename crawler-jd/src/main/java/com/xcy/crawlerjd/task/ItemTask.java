b419b3728bd9e58c114e1d69c6b23407e8c52e8f
xcy <13717736@qq.com>
2020-07-01 15:24:03 +0800

    111

--- crawler-jd/src/main/java/com/xcy/crawlerjd/task/ItemTask.java
+++ crawler-jd/src/main/java/com/xcy/crawlerjd/task/ItemTask.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xcy.crawlerjd.pojo.Item;
 import com.xcy.crawlerjd.service.ItemService;
 import com.xcy.crawlerjd.util.HttpClientUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -30,6 +31,7 @@ public class ItemTask {
 
     @Autowired
     private HttpClientUtils httpClientUtils;
+
     @Autowired
     private ItemService itemService;
 
@@ -40,9 +42,9 @@ public class ItemTask {
     @Scheduled(fixedRate = 100 * 1000)
     public void itemTask() throws Exception {
         //声明需要解析的初始化地址
-        String url = "https://search.jd.com/Search?keyword=手机&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&cid2=653&cid3=655&s=112&click=0&page=";
+        String url = "https://search.jd.com/Search?keyword=%E6%89%8B%E6%9C%BA&suggest=1.def.0.V19--38s0&wq=%E6%89%8B%E6%9C%BA&page=";
         System.out.println("sssssfff");
-        for(int i = 1;i< 10; i=i+2){
+        for (int i = 1; i < 10; i = i + 2) {
             String html = httpClientUtils.doGetHtml(url + i);
             System.out.println(html);
             this.pase(html);
@@ -57,13 +59,17 @@ public class ItemTask {
         Document doc = Jsoup.parse(html);
         //获取spu信息
         Elements spuEles = doc.select("div#J_goodsList > ul > li");
-        for (Element spuEle : spuEles){
-            //获取spu
-            long spu = Long.parseLong(spuEle.attr("data-spu"));
+        for (Element spuEle : spuEles) {
+            String attr = spuEle.attr("data-spu");
+            long spu = 0;
+            if (StringUtils.isNotEmpty(attr)) {
+                //获取spu
+                spu = Long.parseLong(attr);
+            }
 
             //获取sku信息
             Elements skuEles = spuEle.select("li.ps-item");
-            for(Element skuEle : skuEles){
+            for (Element skuEle : skuEles) {
                 //获取sku
                 long sku = Long.parseLong(skuEle.select("[data-sku]").attr("data-sku"));
 
@@ -71,19 +77,19 @@ public class ItemTask {
                 Item item = new Item();
                 item.setSku(sku);
                 List<Item> list = this.itemService.findAll(item);
-                if(list.size() > 0){
+                if (list.size() > 0) {
                     //如果商品存在则进行下一轮循环遍历
                     continue;
                 }
                 //设置商品的spu
                 item.setSpu(spu);
                 //获取商品的详情url
-                String itemUrl = "https://item.jd.com/"+sku+".html";
+                String itemUrl = "https://item.jd.com/" + sku + ".html";
                 item.setUrl(itemUrl);
 
 
                 String picUrl = "http:" + skuEle.select("img[data-sku]").first().attr("data-lazy-img");
-                picUrl = picUrl.replace("/n9/","/n1/");
+                picUrl = picUrl.replace("/n9/", "/n1/");
                 String picName = this.httpClientUtils.doGetImage(picUrl);
                 item.setPic(picName);
 
@@ -104,8 +110,6 @@ public class ItemTask {
                 this.itemService.save(item);
 
             }
-
-
 
 
         }
